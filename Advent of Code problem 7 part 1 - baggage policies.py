a498aad3bc222bac1cbb063ac5cac8d835d55d4d
GB071957 <74923595+GB071957@users.noreply.github.com>
2020-12-07 17:33:59 -0500

    Adding problem 7

---
+++ Advent of Code problem 7 part 1 - baggage policies.py
@@ -0,0 +1,38 @@
+#   Advent of Code problem 7 part 1  Program by Greg Brinks
+
+# Decode Seat Data
+
+
+from timeit import default_timer as timeit
+start= timeit()
+
+import csv, re
+
+
+with open("Advent of Code problem 7 - Baggage Rules.txt") as br_file:
+    file_contents = csv.reader(br_file, delimiter='\n')
+
+    bag_contents = {}
+    for line in file_contents:
+        bag = re.findall(r'(\w+ \w+) bags contain',line[0])
+        contents = re.findall(r'(\d) (\w+ \w+) bag',line[0])
+        d2 = {}
+        for content in contents:
+            d2.update({content[1]:int(content[0])})
+        bag_contents.update({bag[0]:d2})
+        
+    contain_shiny_gold = []
+    for bag,contents in bag_contents.items():
+        if 'shiny gold' in contents:
+            contain_shiny_gold.append(bag)
+    for winner in contain_shiny_gold:
+        for bag,contents in bag_contents.items():
+            if winner in contents and bag not in contain_shiny_gold:
+                contain_shiny_gold.append(bag)
+
+print(f'There are {len(contain_shiny_gold)} bags that contain a shiny gold bag')
+
+end = timeit()
+print(f'Solution time: {end-start}')
+
+
---
+++ Advent of Code problem 7 part 2 - baggage policies.py
@@ -0,0 +1,40 @@
+#   Advent of Code problem 7 part 1  Program by Greg Brinks
+
+# Decode Seat Data
+
+
+from timeit import default_timer as timeit
+start= timeit()
+
+import csv, re
+
+
+with open("Advent of Code problem 7 - Baggage Rules.txt") as br_file:
+    file_contents = csv.reader(br_file, delimiter='\n')
+
+    bag_contents = {}
+    for line in file_contents:
+        bag = re.findall(r'(\w+ \w+) bags contain',line[0])
+        contents = re.findall(r'(\d) (\w+ \w+) bag',line[0])
+        d2 = {}
+        for content in contents:
+            d2.update({content[1]:int(content[0])})
+        bag_contents.update({bag[0]:d2})
+    
+    sbg_contents = []
+    total_bags = 0
+    for bag,num in bag_contents['shiny gold'].items():
+        sbg_contents.append((bag, int(num)))
+        total_bags += int(num)
+    
+    for combo in sbg_contents:
+        for bag,num in bag_contents[combo[0]].items():
+            total_bags += int(num)*combo[1]
+            sbg_contents.append((bag, int(num)*combo[1]))
+            
+print(f'The shiny gold bag contains {total_bags} bags')
+
+end = timeit()
+print(f'Solution time: {end-start}')
+
+
